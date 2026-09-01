92cc809b26b6e3d08a5a19e58b85345ccf6c60f4
Saksham Sinha <sakshamsinha1661@gmail.com>
2026-09-02 03:17:12 +0530

    Time: 2 ms (39.96%), Space: 19.3 MB (63.42%) - LeetHub

--- 3663-find-the-least-frequent-digit/3663-find-the-least-frequent-digit.py
+++ 3663-find-the-least-frequent-digit/3663-find-the-least-frequent-digit.py
@@ -1,14 +1,12 @@
 class Solution:
     def getLeastFrequentDigit(self, n: int) -> int:
         f={}
-        a=float("inf")
         l=[]
         for i in str(n):
             f[i]=f.get(i,0)+1
+        min_freq=min(f.values())
         for i in f:
-            a=min(a,f[i])
-        for i in f:
-            if f[i]==a:
+            if f[i]==min_freq:
                 l.append(int(i))
         return min(l)
         
