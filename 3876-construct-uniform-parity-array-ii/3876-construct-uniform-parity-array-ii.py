100dc4ac5ad132e2ba0835b926ad897230af3df9
Saksham Sinha <sakshamsinha1661@gmail.com>
2026-09-03 10:42:28 +0530

    Time: 70 ms (52.44%), Space: 35.3 MB (73.17%) - LeetHub

---
+++ 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.py
@@ -0,0 +1,12 @@
+class Solution:
+    def uniformArray(self, nums1: list[int]) -> bool:
+        min_odd = float('inf')
+        min_even = float('inf')
+        for x in nums1:
+            if x % 2 == 0:
+                min_even = min(min_even, x)
+            else:
+                min_odd = min(min_odd, x)
+        if min_odd == float('inf'):
+            return True
+        return min_odd < min_even
