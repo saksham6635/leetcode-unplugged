c2c699bc259674debf803081411f04492d5fb7e6
Saksham Sinha <sakshamsinha1661@gmail.com>
2026-09-12 12:40:45 +0530

    Time: 3 ms (33.17%), Space: 19.2 MB (88.49%) - LeetHub

--- 3936-minimum-swaps-to-move-zeros-to-end/3936-minimum-swaps-to-move-zeros-to-end.py
+++ 3936-minimum-swaps-to-move-zeros-to-end/3936-minimum-swaps-to-move-zeros-to-end.py
@@ -1,14 +1,13 @@
 class Solution:
     def minimumSwaps(self, nums: list[int]) -> int:
         zero=nums.count(0)
-        arr1=[0]*zero
-        arr2=nums[-zero:]
-        count=0
-        for i in range(len(arr1)):
-            if arr2[i]!=arr1[i]:
-                count+=1
-        return count
-
+        n=len(nums)
+        swaps=0
+        for i in range(n-zero,n):
+            if nums[i]!=0:
+                swaps+=1
+        return swaps 
+       
 
 
      
